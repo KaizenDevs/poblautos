72ac9fca240c3f49fa667e1b52105fc3642c0a56
alejoescobar <alejoescobac@gmail.com>
2015-08-04 11:22:11 -0500

    Add number column to opinion model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803233328) do
+ActiveRecord::Schema.define(version: 20150804161805) do
 
   create_table "opinions", force: :cascade do |t|
     t.text     "opinion"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150803233328) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "number"
   end
 
   create_table "users", force: :cascade do |t|
