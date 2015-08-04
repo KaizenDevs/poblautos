701d9111c6dba25c9b0d61c8d619f19aa34845eb
alejoescobar <alejoescobac@gmail.com>
2015-08-03 22:16:33 -0500

    Add form to create opinion

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -6,8 +6,33 @@ class OpinionsController < ApplicationController
   end
 
   def new
+    @opinion = Opinion.new
   end
 
   def edit
+  end
+
+  def create
+    @opinion = Opinion.new(opinion_params)
+
+    if @opinion.save
+      flash[:notice] = "La opinión ha sido creada con éxito."
+      redirect_to opinions_path
+    else
+      flash[:alert] = "Ha ocurrido un error y no se ha guardado la opinión. Inténtalo de nuevo."
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def opinion_params
+    params.require(:opinion).permit(:opinion, :name, :position, :avatar)
   end
 end
