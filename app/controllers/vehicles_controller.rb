ab8a2a07df715cee0993e4672e664c3b28e0ae82
alejoescobar <alejoescobac@gmail.com>
2015-08-04 17:51:07 -0500

    Add actions and routes to vehicles

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -5,4 +5,51 @@ class VehiclesController < ApplicationController
 
   def used
   end
+
+  def index
+    @vehicles = Vehicle.all
+  end
+
+  def show
+    @vehicle = Vehicle.find(params[:id])
+  end
+
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def edit
+    @vehicle = Vehicle.find(params[:ids])
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+
+    if @vehicle.create
+      flash[:notice] = "El vehículo ha sido creado éxitosamente."
+      redirect_to vehicles_path
+    else
+      flash[:alert] = "El vehículo no se ha podido crear, inténtalo de nuevo."
+      render 'new'
+    end
+  end
+
+  def update
+    @vehicle = Vehicle.find(params[:id])
+
+    if @vehicle.update(vehicle_params)
+      flash[:notice] = "El vehículo ha sido actualizado éxitosamente."
+      redirect_to vehicles_path
+    else
+      flash[:alert] = "No se ha podido actualizar el vehículo. Inténtalo nuevamente."
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @vehicle = Vehicle.find(params[:id])
+
+    @vehicle.destroy
+    redirect_to vehicles_path
+  end
 end
