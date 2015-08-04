7a63f678b06df1cd6eed9361a2c13479c73cf366
alejoescobar <alejoescobac@gmail.com>
2015-08-04 17:10:35 -0500

    Add paperclip to vehicle model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804215157) do
+ActiveRecord::Schema.define(version: 20150804220253) do
 
   create_table "opinions", force: :cascade do |t|
     t.text     "opinion"
@@ -62,8 +62,12 @@ ActiveRecord::Schema.define(version: 20150804215157) do
     t.string   "color"
     t.string   "license_plate"
     t.text     "comments"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
