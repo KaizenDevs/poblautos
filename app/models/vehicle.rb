85429bf3ede08f990b870c63e67eab68b15ebd86
alejoescobar <alejoescobac@gmail.com>
2015-08-04 20:22:50 -0500

    Initial Vehicle create and index

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -32,8 +32,8 @@
 class Vehicle < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-
   enum transmission: [:mecánico, :automático]
   enum gas: [:gasolina, :gas, :eléctrico]
   enum type: [:camioneta, :sedan, :coupe, :convertible]
+
 end
