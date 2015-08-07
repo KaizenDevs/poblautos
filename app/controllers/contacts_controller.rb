07475ba7a58963e0bd922c4c804233c8f5cd7865
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-08-07 09:54:19 -0500

    Add create action to send contact form to database

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,18 @@
 class ContactsController < ApplicationController
-	def new
-	end
+	
 	def create
-
+		@contact = Contact.new(contact_params)
+		if @contact.save
+  	  # flash[:notice] = "La categoria #{@contact.name} fue creada con exito"
+  	  redirect_to root_path
+  	else
+  		# flash[:alert] = "Ha ocurrido un error y la categoria #{@contact.name}, no ha sido almacenado"
+    	render :action => 'new'
+  	end
 	end
+	
+	private
+		def contact_params
+  		params.require(:contact).permit(:name,:lastname,:email,:phone,:message)
+		end
 end
