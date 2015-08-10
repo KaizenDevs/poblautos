f18f26e7d649e48759b034eabbfd598b94ea3e97
German Hernandez <muygrafico@gmail.com>
2015-08-10 10:52:04 -0500

    basic CRUD (without the R) works - show view ommited, not needed

---
+++ app/controllers/allies_controller.rb
@@ -0,0 +1,36 @@
+class AlliesController < ApplicationController
+  def index
+    @allies = Ally.all
+  end
+  def new
+    @ally = Ally.new
+  end
+  def create
+    @ally = Ally.new(ally_params)
+    @ally.save
+    redirect_to action: "index"
+  end
+  def show
+    @ally = Ally.find(params[:id])
+  end
+  def edit
+    @ally = Ally.find(params[:id])
+  end
+  def update
+    @ally = Ally.find(params[:id])
+    if @ally.update(ally_params)
+      redirect_to action: "index"
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @ally = Ally.find(params[:id])
+    @ally.destroy
+    redirect_to action: "index"
+  end
+  private
+  def ally_params
+    params.require(:ally).permit(:name, :logo, :link)
+  end
+end
