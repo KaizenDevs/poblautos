db127a6101d79350f1892acc7f938cfb85585acc
alejoescobar <alejoescobac@gmail.com>
2015-08-21 08:28:15 -0500

    Fix details

--- app/models/news.rb
+++ app/models/news.rb
@@ -19,6 +19,6 @@
 
 class News < ActiveRecord::Base
   belongs_to :category
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :normal => "600x400>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :normal => "600x400>", :home => "337x224#", convert_options: { home: " -gravity center -crop '337x337+0+0'" } }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
