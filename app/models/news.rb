b793a7f3a64eddc63071208171289eb5310038c1
alejoescobar <alejoescobac@gmail.com>
2015-08-21 10:43:50 -0500

    Final news details

--- app/models/news.rb
+++ app/models/news.rb
@@ -23,4 +23,12 @@ class News < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_attached_file :author_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :author_image, :content_type => /\Aimage\/.*\Z/
+
+  def previous
+    News.limit(1).order("id DESC").where("id < ?", id).first
+  end
+
+  def next
+    News.limit(1).order("id DESC").where("id > ?", id).first
+  end
 end
