d04fb5ba9e6d3b9301561c98cba93433faabf0f9
alejoescobar <alejoescobac@gmail.com>
2015-08-21 15:12:49 -0500

    Fix vehicle images

--- app/models/vehicle_image.rb
+++ app/models/vehicle_image.rb
@@ -1,5 +1,5 @@
   class VehicleImage < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :home => "245x163#",convert_options: { home: " -gravity center -crop '245x245+0+0'" } }, :default_url => "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :home => "245x163#", :show => "170x113#", :main => "703x474#", convert_options: { home: " -gravity center -crop '245x245+0+0'", show: " -gravity center -crop '170x170+0+0'", main: " -gravity center -crop '703x703+0+0'" } }, :default_url => "http://i592.photobucket.com/albums/tt5/Mardini03/imagen-no-disponible.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :vehicle
 end
