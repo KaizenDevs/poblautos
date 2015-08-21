6c26368ecb903be4e980f9565743401d84680f3a
alejoescobar <alejoescobac@gmail.com>
2015-08-21 16:30:07 -0500

    Fix margins

--- app/controllers/procedures_controller.rb
+++ app/controllers/procedures_controller.rb
@@ -1,5 +1,5 @@
 class ProceduresController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:create, :show]
 
   def index
     @procedures = Procedure.all
