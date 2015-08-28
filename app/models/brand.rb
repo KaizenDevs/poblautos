546315cb92daa1b2e1f57ea78652bc18e371f582
alejoescobar <alejoescobac@gmail.com>
2015-08-28 15:42:51 -0500

    Add final revisions

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,5 @@
 class Brand < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :nav_bar => "89x44#", convert_options: { nav_bar: " -gravity center -crop '89x89+0+0'" } }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :nav_bar => "60x60#", convert_options: { nav_bar: " -gravity center -crop '60x60+0+0'" } }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :vehicles, dependent: :destroy
 end
